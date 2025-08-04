b62a19d38673b115558587c8f00848ffeed98dd5
Klavdiya <klavdiya9834@gmail.com>
2025-08-04 19:22:47 +0100

    changes

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,17 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Banner() {
   return (
-    <div className="flex justify-between">
+    <div className="lg:flex lg:justify-between">
       {/* LEFT SIDE */}
-      <div></div>
+      <div className="">
+        <h1 className="font-bold text-4xl text-blue-950">Experience trading like never before</h1>
+        <p className="text-neutral-500 mt-4 text-lg">Use quickfunds globla optimized trading bot which is optimized for maximum profit</p>
+      </div>
+      <div className="mt-12">
+        <Link href="/" className=" rounded h-[50px] py-4 px-8 text-neutral-100 bg-blue-600">Get Started</Link>
+      </div>
       {/* IMAGE SIDE */}
-      <Image
-        src="/images/banner-people.png"
-        alt="My image"
-        width={500}
-        height={500}
-      />
+
+      <div className="h-[450px] md:h-[550px] lg:h-[400px] mt-6">
+        <Image
+          src="/images/banner-people.png"
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="object-contain w-full h-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,31 +1,33 @@
 import Link from "next/link";
+import { MdMenu } from "react-icons/md";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between gap-4 h-[70px] bg-blue-50 sticky top-0 z-50 backdrop-blur py-8 px-32">
+    <nav className="flex justify-between items-center gap-4 h-[50px] bg-indigo-50 sticky top-0 z-50 backdrop-blur pb-8 pt-12 lg:px-32  px-4">
       {/* FIRST SIDE */}
-      <div className="">
-        <h1>BigCakes</h1>
+      <div className="flex items-center gap-1">
+        <div
+          className="bg-blue-800 text-white p-1 h-[30px] w-[30px] flex items-center justify-center rounded-tr-xl rounded-br-none 
+  rounded-tl-none rounded-bl-xl"
+        >
+          <h2 className="text-neutral-100">Q</h2>
+        </div>
+        <h2 className=" text-blue-800 font-semibold">QUICKFUNDS</h2>
       </div>
       {/* SECOND SIDE */}
-      <div className="flex justify-between gap-4">
-        <Link href="/">
-          Services
-        </Link>
-        <Link href="/">
-          Plans
-        </Link>
-        <Link href="/">
-          Affiliates
-        </Link>
-        <Link href="/">
-          About Us
-        </Link>
+      <div className="hidden lg:flex justify-between gap-4">
+        <Link href="/">Services</Link>
+        <Link href="/">Plans</Link>
+        <Link href="/">Affiliates</Link>
+        <Link href="/">About Us</Link>
       </div>
       {/* THIRD SIDE */}
-      <div className="">
+      <div className="hidden lg:flex">
         <Link href="/">Sing In</Link>
         <Link href="/">Get Started</Link>
+      </div>
+      <div className="lg:hidden flex">
+        <MdMenu size={30} className="text-neutral-500"/>
       </div>
     </nav>
   );
