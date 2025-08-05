f21ad10d6ee5dc5871bfdb81f1d377c8b98058a9
Klavdiya <klavdiya9834@gmail.com>
2025-08-05 15:00:59 +0100

    updates

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,30 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaPlay } from "react-icons/fa6";
+
 
 export default function Banner() {
   return (
-    <div className="lg:flex lg:justify-between py-12 bg-neutral-50 px-4 md:px-4 lg:px-32 border-blue-50">
+    <div className="lg:grid lg:grid-cols-2 items-center py-12 lg:pt-4 lg:pb-12 bg-neutral-50 px-4 md:px-4 lg:px-32 ">
+      <div>
+        <div className="">
+          <h1 className="font-bold text-4xl lg:text-5xl lg:max-w-[500px]  lg:leading-[55px] text-blue-900 leading-[45px]">
+            Experience trading like never before
+          </h1>
+          <p className="text-slate-600 mt-4 text-lg lg:text-xl  leading-[30px]">
+            Use quickfunds global optimized trading bot which is optimized for
+            maximum profit.
+          </p>
+        </div>
+        <div className="mt-12 mb-16 flex gap-10 items-center">
+          <div>
+            <Link
+              href="/"
+              className=" rounded h-[50px] py-4 px-8 text-neutral-50 font-semibold bg-blue-600 shadow-xl"
+            >
+              Get Started
+            </Link>
+          </div>
+          <div className="flex gap-4 items-center text-blue-500 cursor-pointer">
+            <div className="w-[45px] h-[45px] font-semibold border-2 border-blue-500 rounded-full flex justify-center items-center">
+                <FaPlay className="bg-blue-500"/>
+            </div>
+            <div className="font-semibold">How it works</div>
+          </div>
+        </div>
+      </div>
       {/* LEFT SIDE */}
-      <div className="">
-        <h1 className="font-bold text-4xl text-blue-900">
-          Experience trading like never before
-        </h1>
-        <p className="text-slate-600 mt-4 text-lg">
-          Use quickfunds global optimized trading bot which is optimized for
-          maximum profit and make more money than you ever could realistically imagine
-        </p>
-      </div>
-      <div className="mt-12 mb-16">
-        <Link
-          href="/"
-          className=" rounded h-[50px] py-4 px-8 text-neutral-100 bg-blue-600 shadow-xl"
-        >
-          Get Started
-        </Link>
-      </div>
       {/* IMAGE SIDE */}
 
-      <div className="h-[450px] md:h-[550px] lg:h-[400px] relative mt-6">
+      <div className="h-[450px] md:h-[550px] lg:h-[500px] relative mt-6 lg:mt-2">
         <Image
           src="/images/banner-people.png"
           alt=""
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -16,33 +16,38 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`flex justify-between items-center sticky top-0 z-50 px-4 lg:px-32 py-8 backdrop-blur transition-all duration-300 ${
-        scrolled ? "bg-white shadow-md" : "bg-neutral-50"
+      className={`flex justify-between items-center sticky py-6 top-0 z-50 px-4 lg:px-32 backdrop-blur transition-all duration-300 ${
+        scrolled ? "bg-white shadow-md" : "bg-neutral-50 "
       }`}
     >
       {/* FIRST SIDE */}
       <div className="flex items-center gap-1">
         <div
-          className="bg-blue-800 text-white p-1 h-[30px] w-[30px] flex items-center justify-center rounded-tr-xl rounded-br-none 
+          className="bg-blue-900 text-white p-1 h-[30px] w-[30px] flex items-center justify-center rounded-tr-xl rounded-br-none 
   rounded-tl-none rounded-bl-xl"
         >
           <h2 className="text-neutral-100 text-xl">Q</h2>
         </div>
-        <h2 className=" text-blue-800 font-semibold text-xl">
-          QUICKFUNDS
-        </h2>
+        <h2 className=" text-blue-900 font-semibold text-xl">QUICKFUNDS</h2>
       </div>
       {/* SECOND SIDE */}
-      <div className="hidden lg:flex justify-between gap-4 text-neutral-600 font-semibold text-[15px]">
+      <div className="hidden lg:flex justify-between gap-8  text-slate-500 font-semibold">
         <Link href="/">Services</Link>
         <Link href="/">Plans</Link>
         <Link href="/">Affiliates</Link>
         <Link href="/">About Us</Link>
       </div>
       {/* THIRD SIDE */}
-      <div className="hidden lg:flex">
-        <Link href="/">Sing In</Link>
-        <Link href="/">Get Started</Link>
+      <div className="hidden lg:flex gap-8 items-center">
+        <Link href="/" className="text-slate-500 font-semibold">
+          Sign In
+        </Link>
+        <Link
+          href="/"
+          className=" rounded h-[50px] py-4 px-8 text-neutral-100 bg-blue-600 shadow-xl font-semibold"
+        >
+          Get Started
+        </Link>
       </div>
       <div className="lg:hidden flex">
         <MdMenu size={30} className="text-neutral-500" />
