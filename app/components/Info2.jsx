a6c4a95a04bea0aa90a080e2118655aa7dc8a4cf
Klavdiya <klavdiya9834@gmail.com>
2025-08-06 15:02:25 +0100

    updates

--- app/components/Info2.jsx
+++ app/components/Info2.jsx
@@ -4,12 +4,12 @@ import Wrapper from "./Wrapper";
 export default function Info2() {
   return (
     <div className="py-12 lg:pt-4 lg:pb-12 bg-neutral-50 px-5 md:px-8 lg:px-32 h-[500px]">
-      <h2 className="text-center text-2xl font-semibold text-[#0b163f]">
+      <h2 className="text-center text-2xl font-bold text-[#0b163f]">
         Over 500 Companies Trusted Us
       </h2>
       <div className="grid grid-cols-2 gap-6 mt-6">
         <div className="h-[70px] p-1 md:h-[250px] lg:h-[200px] relative bg-white flex items-center justify-center rounded-tr-none rounded-br-xl 
-  rounded-tl-xl rounded-bl-none">
+  rounded-tl-xl rounded-bl-none shadow-sm">
           <Image
             src="/images/huobi.svg"
             alt=""
@@ -20,7 +20,7 @@ export default function Info2() {
           />
         </div>
         <div className="h-[70px] p-3 md:h-[250px] lg:h-[200px] relative bg-white flex items-center justify-center rounded-tr-none rounded-br-xl 
-  rounded-tl-xl rounded-bl-none">
+  rounded-tl-xl rounded-bl-none shadow-sm">
           <Image
             src="/images/coinone.svg"
             alt=""
@@ -31,7 +31,7 @@ export default function Info2() {
           />
         </div>
         <div className="h-[70px] p-3 md:h-[250px] lg:h-[200px] relative bg-white flex items-center justify-center rounded-tr-none rounded-br-xl 
-  rounded-tl-xl rounded-bl-none">
+  rounded-tl-xl rounded-bl-none shadow-sm">
           <Image
             src="/images/montex.svg"
             alt=""
@@ -42,7 +42,7 @@ export default function Info2() {
           />
         </div>
         <div className="h-[70px] p-3 md:h-[250px] lg:h-[200px] relative bg-white flex items-center justify-center rounded-tr-none rounded-br-xl 
-  rounded-tl-xl rounded-bl-none">
+  rounded-tl-xl rounded-bl-none shadow-sm">
           <Image
             src="/images/the-blockchain-group.svg"
             alt=""
