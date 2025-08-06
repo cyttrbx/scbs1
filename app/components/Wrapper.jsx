6210a8cdc1f1b69519644502d867991d84469488
Klavdiya <klavdiya9834@gmail.com>
2025-08-06 12:08:57 +0100

    updates

--- app/components/Wrapper.jsx
+++ app/components/Wrapper.jsx
@@ -1,3 +1,3 @@
 export default function Wrapper({ children }) {
-  return <div className="px-4 xl:px-20 md:px-10 py-10 md:py-8 overflow-y-hidden">{children}</div>;
+  return <div className="px-4 xl:px-20 md:px-10 py-10 md:py-8">{children}</div>;
 }
